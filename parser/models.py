6859ebf7cfdd11a0ab602e4181832dd2c57cea3b
SONGSERI <seri.song@wizcore.co.kr>
2026-01-31 10:31:52 +0700

    Update models.py

--- parser/models.py
+++ parser/models.py
@@ -1,20 +1,21 @@
-# models.py
 from dataclasses import dataclass
+from datetime import datetime
+from typing import Optional
 
 @dataclass
 class Lot:
     lot_id: str
     lot_name: str
-    start_time: str | None = None
-    end_time: str | None = None
-    lane: str | None = None
+    start_time: Optional[datetime] = None
+    end_time: Optional[datetime] = None
+    lane: Optional[str] = None
 
 @dataclass
 class Machine:
     machine_id: str
     line_id: str
-    stage_no: str
-    machine_order: str
+    stage_no: int
+    machine_order: int
 
 @dataclass
 class LotMachine:
