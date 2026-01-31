100effff5f2952cb19e6d9253f4e75e256a2c65a
SONGSERI <seri.song@wizcore.co.kr>
2026-01-31 10:41:18 +0700

    Create insert_u03.py

---
+++ parser/sql/insert_u03.py
@@ -0,0 +1,62 @@
+from db import execute_upsert
+
+SQL_PICKUP_SUMMARY = """
+INSERT INTO pickup_error_summary (
+    lot_machine_id,
+    total_pickup_count, total_error_count,
+    pickup_error_count, recognition_error_count,
+    thick_error_count, placement_error_count,
+    part_drop_error_count, transfer_unit_part_drop_error_count,
+    pre_pickup_inspection_error_count
+)
+VALUES (
+    %(lot_machine_id)s,
+    %(total_pickup_count)s, %(total_error_count)s,
+    %(pickup_error_count)s, %(recognition_error_count)s,
+    %(thick_error_count)s, %(placement_error_count)s,
+    %(part_drop_error_count)s, %(transfer_unit_part_drop_error_count)s,
+    %(pre_pickup_inspection_error_count)s
+)
+ON CONFLICT (lot_machine_id)
+DO UPDATE SET
+    total_pickup_count = EXCLUDED.total_pickup_count,
+    total_error_count = EXCLUDED.total_error_count
+"""
+
+SQL_COMPONENT = """
+INSERT INTO component (
+    component_id, machine_id, table_id, feeder_id, feeder_serial,
+    nozzle_changer, nozzle_holder, nozzle_serial,
+    part_number, library_name
+)
+VALUES (
+    %(component_id)s, %(machine_id)s, %(table_id)s, %(feeder_id)s, %(feeder_serial)s,
+    %(nozzle_changer)s, %(nozzle_holder)s, %(nozzle_serial)s,
+    %(part_number)s, %(library_name)s
+)
+ON CONFLICT (component_id) DO NOTHING
+"""
+
+SQL_COMPONENT_SUMMARY = """
+INSERT INTO component_pickup_summary (
+    lot_machine_id, component_id,
+    pickup_count, error_count,
+    pickup_error_count, recognition_error_count,
+    source_file_id
+)
+VALUES (
+    %(lot_machine_id)s, %(component_id)s,
+    %(pickup_count)s, %(error_count)s,
+    %(pickup_error_count)s, %(recognition_error_count)s,
+    %(source_file_id)s
+)
+ON CONFLICT (lot_machine_id, component_id)
+DO UPDATE SET
+    pickup_count = EXCLUDED.pickup_count,
+    error_count = EXCLUDED.error_count
+"""
+
+def insert_u03(conn, pickup_summary, components, component_summaries):
+    execute_upsert(conn, SQL_PICKUP_SUMMARY, [pickup_summary])
+    execute_upsert(conn, SQL_COMPONENT, components)
+    execute_upsert(conn, SQL_COMPONENT_SUMMARY, component_summaries)
