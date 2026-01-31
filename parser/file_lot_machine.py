ceede8e604037bdae72d86c1fd51bc8483e8b7b9
SONGSERI <seri.song@wizcore.co.kr>
2026-01-31 10:32:22 +0700

    Create file_lot_machine.py

---
+++ parser/file_lot_machine.py
@@ -0,0 +1,7 @@
+# ERD: FILE_LOT_MACHINE
+
+def make_file_lot_machine(file_id: str, lot_machine_id: str) -> dict:
+    return {
+        "file_id": file_id,
+        "lot_machine_id": lot_machine_id,
+    }
