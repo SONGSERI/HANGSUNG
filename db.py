0be5deb83907f185d85f85fd97e36ff6511440a5
SONGSERI <seri.song@wizcore.co.kr>
2026-02-04 17:32:41 +0700

    Create db.py

---
+++ db.py
@@ -0,0 +1,17 @@
+import pandas as pd
+from sqlalchemy import create_engine
+
+
+def get_engine(
+    user="song",
+    password="a12345",
+    host="localhost",
+    port=5432,
+    dbname="song",
+):
+    url = f"postgresql+psycopg2://{user}:{password}@{host}:{port}/{dbname}"
+    return create_engine(url)
+
+
+def load_table(engine, table_name: str) -> pd.DataFrame:
+    return pd.read_sql(f"SELECT * FROM {table_name}", engine)
