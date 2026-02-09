a847b112a17848a193c1ba9efda4ef5b94edb5ba
srsong <seri.song@wizcore.co.kr>
2026-02-09 17:39:13 +0700

    tag

--- parser/sql/db.py
+++ parser/sql/db.py
@@ -3,16 +3,18 @@
 # 방식: UPSERT(중복 허용, 재실행 안전)
 
 
+import os
+
 import psycopg2
 from psycopg2.extras import execute_batch
 
 def get_conn():
     return psycopg2.connect(
-        host="localhost",
-        port=5432,
-        dbname="song",
-        user="song",
-        password="",
+        host=os.getenv("DB_HOST", "localhost"),
+        port=int(os.getenv("DB_PORT", "5432")),
+        dbname=os.getenv("DB_NAME", "song"),
+        user=os.getenv("DB_USER", "song"),
+        password=os.getenv("DB_PASSWORD", ""),
     )
 
 def execute_upsert(conn, sql, rows):
