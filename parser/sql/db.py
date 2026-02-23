d834c7f0b0ed6810819115f754bc8bebc4b05aad
SONGSERI <seri.song@wizcore.co.kr>
2026-02-23 16:36:10 +0700

    Update db.py

--- parser/sql/db.py
+++ parser/sql/db.py
@@ -10,11 +10,11 @@ from psycopg2.extras import execute_batch
 
 def get_conn():
     return psycopg2.connect(
-        host=os.getenv("DB_HOST", "localhost"),
+        host=os.getenv("DB_HOST", "192.168.200.105"),
         port=int(os.getenv("DB_PORT", "5432")),
-        dbname=os.getenv("DB_NAME", "song"),
-        user=os.getenv("DB_USER", "song"),
-        password=os.getenv("DB_PASSWORD", ""),
+        dbname=os.getenv("DB_NAME", "nexedge"),
+        user=os.getenv("DB_USER", "analysis"),
+        password=os.getenv("DB_PASSWORD", "analysis1!"),
     )
 
 def execute_upsert(conn, sql, rows):
